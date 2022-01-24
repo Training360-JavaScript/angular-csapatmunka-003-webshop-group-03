8ae5f4cd9e97d31827a2e5740cbd24359dfee628
Gábor Vass <90134776+Vassgabi89@users.noreply.github.com>
2022-01-24 14:14:04 +0100

    main

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,3 +1,8 @@
+import { Component, OnInit } from "@angular/core";
+import { Product } from "src/app/model/product";
+import { CategoryService } from "src/app/service/category.service";
+import { ProductService } from "src/app/service/product.service";
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -28,7 +33,7 @@ export class HomeComponent implements OnInit {
 
 
   // A product tömb kategóriával szűrt tartalma. A szűrési változó a kategória neve.
-  listOfCategorizedProducts: Product[] = this.productService.getCategorized('comedy');
+  //listOfCategorizedProducts: Product[] = this.productService.getCategorized('comedy');
   //listOfCategorizedProducts: Product[] = this.productService.getCategorized('comedy');
 
 }
