9ed06a105eeeda92ea3e381634a4d5a02fedf55d
Gábor Vass <90134776+Vassgabi89@users.noreply.github.com>
2022-01-24 14:15:31 +0100

    like main

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Product } from 'src/app/model/product';
-import { ProductService } from 'src/app/service/product.service';
+import { Component, OnInit } from "@angular/core";
+import { Product } from "src/app/model/product";
+import { CategoryService } from "src/app/service/category.service";
+import { ProductService } from "src/app/service/product.service";
 
 @Component({
   selector: 'app-home',
@@ -9,17 +10,30 @@ import { ProductService } from 'src/app/service/product.service';
 })
 export class HomeComponent implements OnInit {
 
+  phrase: string = '';
+
+  filterKey: string = '';
+
   constructor(
     private productService : ProductService,
+    private categoryService : CategoryService
   ) { }
 
   ngOnInit(): void {
   }
-
   // A product tömb teljes tartalma
-  //listOfProducts: Product[] = this.productService.getAll();
+  listOfProducts: Product[] = this.productService.getAll();
+
+  categoryDetails = this.categoryService.getAllCategoryName();
+
+  probData = this.categoryService.getCategoryDetailes('Horror').description;
+
+  //categoryDetails: String = this.categoryService.getCategoryDetailes('Horror').description;
+  //categoryDetails_2 = this.categoryService.getCategoryDetailes('Horror').id;
+
 
   // A product tömb kategóriával szűrt tartalma. A szűrési változó a kategória neve.
-  //listOfCategorizedProducts: Product[] = this.productService.getCategorized('');
+  //listOfCategorizedProducts: Product[] = this.productService.getCategorized('comedy');
+  //listOfCategorizedProducts: Product[] = this.productService.getCategorized('comedy');
 
 }
